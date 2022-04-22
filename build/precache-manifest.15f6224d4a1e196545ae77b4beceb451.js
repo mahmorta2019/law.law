129f24e77d7f34414c6500ac6478cbde4f4708ae
mahmorta2019 <52012462+mahmorta2019@users.noreply.github.com>
2022-04-22 06:52:23 -0400

    Add files via upload

---
+++ build/precache-manifest.15f6224d4a1e196545ae77b4beceb451.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "129bc8cc6d45b1ccfe1c",
+    "url": "/static/css/main.6b2adba9.chunk.css"
+  },
+  {
+    "revision": "129bc8cc6d45b1ccfe1c",
+    "url": "/static/js/main.129bc8cc.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "8f08900b575fde58c856",
+    "url": "/static/css/2.4943c6c8.chunk.css"
+  },
+  {
+    "revision": "8f08900b575fde58c856",
+    "url": "/static/js/2.8f08900b.chunk.js"
+  },
+  {
+    "revision": "43928d61158871188870",
+    "url": "/static/js/runtime~main.43928d61.js"
+  },
+  {
+    "revision": "78bff76afc4d37e4ea95d3e9512146c0",
+    "url": "/index.html"
+  }
+];
